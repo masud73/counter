ba4a31cb76a882722c71092ad00bd5b32c91a227
masud <mohdabbakarr@gmail.com>
2022-07-07 16:14:16 +0100

    Updates

---
+++ precache-manifest.fde1b8b638f9e3e7cf484fe0132391ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "41d224c7c8693a40afbb",
+    "url": "/counter/static/css/main.af5d2d90.chunk.css"
+  },
+  {
+    "revision": "41d224c7c8693a40afbb",
+    "url": "/counter/static/js/main.41d224c7.chunk.js"
+  },
+  {
+    "revision": "ae88608762ae0ecdeaa7",
+    "url": "/counter/static/js/1.ae886087.chunk.js"
+  },
+  {
+    "revision": "39339da69a09f04205f2",
+    "url": "/counter/static/js/runtime~main.39339da6.js"
+  },
+  {
+    "revision": "93882c26f4ab00626bb0d18318cec4b1",
+    "url": "/counter/index.html"
+  }
+];
